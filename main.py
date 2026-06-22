cf59635c393112534e97a376af7b138f583d3a3b
godobito745-prog <vankardhruvya18@gmail.com>
2026-06-22 07:30:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -265,18 +265,19 @@ async def unmute(update, context):
     await context.bot.restrict_chat_member(
         chat_id=chat_id,
         user_id=user_id,
-        permissions = ChatPermissions(
-    can_send_messages=True,
-    can_send_audios=True,
-    can_send_documents=True,
-    can_send_photos=True,
-    can_send_videos=True,
-    can_send_video_notes=True,
-    can_send_voice_notes=True,
-    can_send_polls=True,
-    can_send_other_messages=True,
-    can_add_web_page_previews=True,
+        permissions=ChatPermissions(
+            can_send_messages=True,
+            can_send_audios=True,
+            can_send_documents=True,
+            can_send_photos=True,
+            can_send_videos=True,
+            can_send_video_notes=True,
+            can_send_voice_notes=True,
+            can_send_polls=True,
+            can_send_other_messages=True,
+            can_add_web_page_previews=True,
         )
+    )
 
     await update.message.reply_text("wow.....now you can speak 🗣️.")
 
